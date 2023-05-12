d517bf7ce1ea522746586754293856bb873b1662
Hydrogen <kenhydrogen@gmail.com>
2023-05-13 00:24:04 +0200

    docs: add jsdocs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { TimestampResult } from "./typings/result";
 
+/**
+ * Parses a timestamp and returns its value in seconds
+ * 
+ * @param timestamp 
+ * @returns {number} value in seconds, or null
+ */
 export function parseTimestamp(timestamp: string): number | null {
+	// we'll split the string based on the colon that separates hours, minutes, and seconds
 	let split: string[] = timestamp.split(':');
 	
+	// fail silently
 	if (split.length < 2 || split.length > 3) {
 		console.warn(`Unexpected timestamp format at ${timestamp}`)
 		
@@ -26,6 +34,12 @@ export function parseTimestamp(timestamp: string): number | null {
 	return seconds;
 }
 
+/**
+ * Returns a WebVTT friendly timestamp.
+ * 
+ * @param timestamp
+ * @returns {string} WebVTT friendly timestamp
+ */
 function handleVttTimestamp(timestamp: string): string {
 	const split: string[] = timestamp.split(':');
 	
@@ -36,6 +50,7 @@ function handleVttTimestamp(timestamp: string): string {
 	for (let part of split) {
 		index++;
 		
+		// add a 0 before the number
 		part = part.length == 1 ? `0${part}` : part;
 		
 		reconstructed += `${part}${index == split.length ? '' : ':'}`;
@@ -48,7 +63,16 @@ function handleVttTimestamp(timestamp: string): string {
 	return `00:${reconstructed}`;
 }
 
+/**
+ * Writes the WebVTT cues.
+ * 
+ * @param timestamps array of TimestampResults @see parseText
+ * @param duration total duration of the video
+ * @returns {string} chaptuer cues in WebVTT
+ */
 function writeCues(timestamps: TimestampResult[], duration: number): string {
+	// converts the duration into a timestamp with milliseconds
+	// i.e. 00:01:42.41
 	const endTimestamp = new Date(duration * 1000).toISOString().slice(11, 22);
 	
 	let vtt: string = '';
@@ -58,25 +82,27 @@ function writeCues(timestamps: TimestampResult[], duration: number): string {
 	for (const timestamp of timestamps) {
 		index++;
 		
-		let end: string;
-		
-		if (index == timestamps.length) {
-			end = endTimestamp;
-		} else end = handleVttTimestamp(timestamps[index].timestamp);
-		
+		const end: string = index == timestamps.length ? endTimestamp : handleVttTimestamp(timestamps[index].timestamp);
+
 		vtt += `${handleVttTimestamp(timestamp.timestamp)} --> ${end}\n${timestamp.name}\n\n`;
 	}
 	
 	return vtt;
 }
 
+/**
+ * Parses text (video description) into TimestampResults.
+ * 
+ * @see TimestampResult
+ * @param description the text to parse chapters from
+ * @returns {TimestampResult[]} array of TimestampResult (name, timestamp, seconds)
+ */
 function parseText(description: string): TimestampResult[] {
+	// the format we're looking for is
+	// 0:13 Chapter Name
 	const lines: string[] = description.split('\n');
 	
 	const timestamps: TimestampResult[] = [];
-	
-	// the format we're looking for is
-	// 0:13 Chapter Name
 	
 	for (const line of lines) {
 		const split = line.split(' ');
@@ -109,11 +135,19 @@ function parseText(description: string): TimestampResult[] {
 	return timestamps;
 }
 
+/**
+ * Generates a WebVTT file from a video description.
+ * 
+ * @param description the text to parse chapters from
+ * @param duration total duration of the video
+ * @returns {string} WebVTT file of chapters
+ */
 export default function generateVtt(description: string, duration: number): string {
 	let result: TimestampResult[] = parseText(description)
-		.filter(t => t.seconds <= duration)
-		.sort((a: TimestampResult, b: TimestampResult) => a.seconds - b.seconds);
+		.filter(t => t.seconds <= duration) // remove chapters defined after the video's over
+		.sort((a: TimestampResult, b: TimestampResult) => a.seconds - b.seconds); // sort chapters lowest to highest
 	
+	// define the WebVTT header and add the chapter cues
 	let vtt = `WEBVTT - from chapter-vtt\n\n${writeCues(result, duration)}`;
 	
 	return vtt;
--- src/typings/result.ts
+++ src/typings/result.ts
@@ -1,5 +1,17 @@
+/**
+ * A chapter definition
+ */
 export interface TimestampResult {
+	/**
+	 * Name of the chapter as defined after the timestamp (00:01 Chapter Name)
+	 */
 	name: string;
+	/**
+	 * Value of the timestamp (00:01) in seconds
+	 */
 	seconds: number;
+	/**
+	 * The timestamp value (00:01)
+	 */
 	timestamp: string;
 }
